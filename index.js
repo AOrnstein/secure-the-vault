d7ef9ea3ea98f092bafd9ecb5c3d1e66013a8942
AOrnstein <ornstein.adam@gmail.com>
2026-04-20 10:42:53 -0600

    Stricter variable declarations

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var code1; // Q1
+let code1; // Q1
 code1 = 7 + 3; // Q2 == 10
 
-var code2 = (100 - 20) / 2; // Q3 == 40
-var code3 = 30 + (19 % 10); // Q4 == 39
+const code2 = (100 - 20) / 2; // Q3 == 40
+const code3 = 30 + (19 % 10); // Q4 == 39
 
-var message = "The vault has been secured. The combination is: "; // Q5
+const message = "The vault has been secured. The combination is: "; // Q5
 
-var codeA = code1 + "-" + code2 + "-" + code3; // Q6 == "10-40-39"
-var codeB = `${code1}-${code2}-${code3}`; // Q7 == "10-40-39"
+const codeA = code1 + "-" + code2 + "-" + code3; // Q6 == "10-40-39"
+const codeB = `${code1}-${code2}-${code3}`; // Q7 == "10-40-39"
 
 console.log(message, codeA, codeB); // Q8
